1790171996ad0add91f22e34ebf364fbf76fa24f
Petrrrrrr573 <dvorakpetr573@gmail.com>
2022-12-26 17:44:33 +0100

    přídán praw reddit

--- main2.py
+++ main2.py
@@ -1,4 +1,11 @@
 import bot
+import praw
 
 if __name__ == '__main__':
      bot.run_discord_bot(token="")
+
+reddit = praw.Reddit(client_id = "",
+                    client_secret = "",
+                    username = "",
+                    password = "",
+                    user_agent = "")
